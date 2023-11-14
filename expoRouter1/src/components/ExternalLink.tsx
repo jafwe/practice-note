0c7ea68612f837bbb0275de3e9470f007ac083b6
Jeff <j0e2f1f6@gmail.com>
2023-11-14 12:36:17 +0800

    test for gpg

--- expoRouter1/src/components/ExternalLink.tsx
+++ expoRouter1/src/components/ExternalLink.tsx
@@ -3,7 +3,6 @@ import * as WebBrowser from 'expo-web-browser'
 import React from 'react'
 import { Platform } from 'react-native'
 
-
 export function ExternalLink(props: Omit<React.ComponentProps<typeof Link>, 'href'> & { href: string }) {
   return (
     <Link
